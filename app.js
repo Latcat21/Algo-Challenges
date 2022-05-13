e2ab293868a41cdc9ac7dbaf94c73a9f7358ab3d
Brian <Brian@Brians-MacBook-Air.local>
2022-05-13 00:15:57 -0500

    seak and destroy javascript solved

--- app.js
+++ app.js
@@ -8,4 +8,20 @@ function addAll(...args){
  
   return total
 }
-console.log(addAll(2,5,6,7))
+
+// Seek and Destroy
+// remove from the array whatever is in the folling arguments return the leftovers in an array
+// ex seekAndDestroy([2,3,4,6,6 'hello'], 2,6) == [3,4 'hello']
+function seekAndDestroy(arr,...args){
+  let final = []
+  
+  for(let i = 0; i <= arr.length; i++){
+    if(!args.includes(arr[i]) && arr[i] !== undefined){
+      final.push(arr[i])
+    }
+  }
+  return final
+}
+
+
+console.log(seekAndDestroy([2,3,4,6,6, 'hello'], 2, 6))
