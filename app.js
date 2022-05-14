f19b675d3134d88c94ae65a56ab23ad16c7780df
Brian <Brian@Brians-MacBook-Air.local>
2022-05-14 18:16:51 -0500

    sort by height solved in js

--- app.js
+++ app.js
@@ -23,5 +23,30 @@ function seekAndDestroy(arr,...args){
   return final
 }
 
+//sort by height 
+// some people are standing in a row in a park, there are trees between them which cannot be moved. your task is to rearrange the people by the their heights in a non-descending order without moving the treets
+// ex.
+// a = [-1,150, 190, 170, -1,-1,160,180]
+// sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
+function sortByHeight(arr){
+  const arr1 = [];
+  const arr2 = [];
 
-console.log(seekAndDestroy([2,3,4,6,6, 'hello'], 2, 6))
+  arr.forEach((ele, i) => {
+    if(ele === -1){
+      arr1.push(i)
+      }else{
+      arr2.push(ele)
+    }
+  });
+
+ let sortedArr2 = arr2.sort((a, b) => a - b);
+ 
+ arr1.forEach((val, i) => sortedArr2.splice(arr1[i], 0, -1));
+
+ return sortedArr2
+
+
+}
+console.log(sortByHeight([-1,150, 190, 170, -1,-1,160,180]))
+
